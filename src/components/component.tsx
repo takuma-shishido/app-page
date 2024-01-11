3fea197165dee2c237d71b1291d886313525087e
shishido-takuma <130330524+takuma-shishido@users.noreply.github.com>
2024-01-11 12:50:16 +0900

    fix image

--- src/components/component.tsx
+++ src/components/component.tsx
@@ -71,7 +71,7 @@ export default function Component() {
               alt="Image"
               className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
               height="310"
-              src="/placeholder.png"
+              src="/app-page/placeholder.png"
               width="550"
             />
           </div>
